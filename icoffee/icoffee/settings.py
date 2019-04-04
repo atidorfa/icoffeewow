766cc23c00101813ea97c88a926435409f9f75da
burrito <dilusionalbanter@gmail.com>
2019-04-04 23:11:01 +0000

    settings.py change

--- icoffee/icoffee/settings.py
+++ icoffee/icoffee/settings.py
@@ -27,9 +27,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['www.icoffee.com.ar', '45.56.66.173']
+ALLOWED_HOSTS = [
+    'icoffee.com.ar',
+    'www.icoffee.com.ar',
+    '45.56.66.173',
+]
 
 
 # Application definition
