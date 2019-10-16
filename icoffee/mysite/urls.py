cf344d6733de764bb765837ea63e6ff1e5a5ea86
dilusionalbanter <dilusionalbanter@gmail.com>
2019-10-16 18:03:43 -0300

    implementacion filtro

--- icoffee/mysite/urls.py
+++ icoffee/mysite/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('mercadolibre', views.mercadolibre, name='mercadolibre'),
     path('facebook', views.facebook, name='facebook'),
     path('pagamenos', views.pagamenos, name='pagamenos'),
-    path('wow', views.wow, name='wow')
+    path('wow', views.wow, name='wow'),
+    path('wow_classic_gold', views.wow_classic_gold, name='wow_classic_gold'),
+    path('offer', views.offer, name='offer')
 ]
